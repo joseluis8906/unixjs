0d5b5de6da2b97619f856e8c108454546362d30c
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-01-08 11:29:48 -0500

    update

--- gitup.sh
+++ gitup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-cd ./frontend/
-./make_unixjs.sh
-cd ../
+function compile {
+    cd ./frontend/
+    ./make_unixjs.sh
+    cd ../
+}
 
 if [ "$1" = "local" ];
 then
+    compile
     git add .
     git commit -m "update"
 elif [ "$1" = "remote" ];
