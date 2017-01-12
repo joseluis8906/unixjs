f0776772814716ccda91f679ec142d027b9e48f2
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-01-11 23:43:38 -0500

    update

--- frontend/lib/gwt/gwt.js
+++ frontend/lib/gwt/gwt.js
@@ -3939,7 +3939,7 @@ Gwt.Gui.KnobThreeLevels.prototype.GetElement = function (Id)
 
 Gwt.Gui.KnobThreeLevels.prototype.SetRotation = function (Angle)
 {
-    var Center = {'X': this.Graphic.GetWidth()/2, 'Y': this.Graphic.GetHeight()/2};
+    var Center = {'X': this.Graphic.GetHtml().getAttribute ("Width")/2, 'Y': this.Graphic.GetHtml().getAttribute("height")/2};
     var str = "rotate(%angle, %x, %y)".replace("%angle", Angle).replace ("%x", Center.X).replace ("%y", Center.Y);
     console.log (str);
     this.Knob.GetHtml().setAttribute ("transform", str);
