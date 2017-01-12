546532ed274038e20993b43fa9de3f816a02b2fe
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-01-12 12:09:29 -0500

    update

--- frontend/bin/domotictrl.js
+++ frontend/bin/domotictrl.js
@@ -23,7 +23,7 @@ function domotictrl ()
     this.Add (this.Layout);
     
     this.Col1.SetBackgroundImage (Gwt.Core.Contrib.Images + "blurry_background2_66.png");
-    this.Col1.SetBackgroundSize (this.Col1.GetWidth (), this.Col1.GetHeight ());
+    this.Col1.SetBackgroundSize (Gwt.Gui.Contrib.BackgroundSize.Cover);
     this.Col1.SetAlignment (Gwt.Gui.ALIGN_CENTER);
     this.Layout.Add (this.Col1);    
     
