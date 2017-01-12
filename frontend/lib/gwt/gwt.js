43e1d0878f3e8183bef3c8b8d6435a11a170a1a5
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-01-11 21:31:23 -0500

    update

--- frontend/lib/gwt/gwt.js
+++ frontend/lib/gwt/gwt.js
@@ -904,7 +904,7 @@ Gwt.Gui.Frame.prototype.SetHtml = function (Element)
         this.Html = Element;
     }
     console.log (this.Html)
-    this.InitStyle ();
+    //this.InitStyle ();
 }
 Gwt.Gui.Frame.prototype.SetTabIndex = function (TabIndex)
 {
