7f4fa50fdaac713b2eced9712b58fc6cd6ac9da7
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-01-11 19:53:35 -0500

    update

--- frontend/bin/bin.js
+++ frontend/bin/bin.js
@@ -54,10 +54,18 @@ function domotictrl ()
 {
     Gwt.Gui.Window.call (this, "Control De Domótica");
 	
+    
+    this.Knob = Gwt.Gui.KnobThreeLevels ();
+    
     this.SetSize (640, 480);
     this.SetPosition (Gwt.Gui.WIN_POS_CENTER);
     
     this.DisableMenu ();
+    
+    //color rgba(48, 40, 40, 0.9);
+    
+    this.Add (this.Knob);
+    
 }
 
 domotictrl.prototype = new Gwt.Gui.Window ();
