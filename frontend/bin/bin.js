a3022d286fc0590ac6cce88900d4042c769ae310
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-01-13 14:07:06 -0500

    update

--- frontend/bin/bin.js
+++ frontend/bin/bin.js
@@ -46,134 +46,394 @@ return new function ()
 	}
 }
 })();
-domotictrl = ( function ()
+login = (function ()
 {
-function PanelItem (Width, Height, Text, Image)
-{
-    Gwt.Gui.Frame.call (this);
-    
-    this.Layout = new Gwt.Gui.HBox (12);
-    this.Text = new Gwt.Gui.StaticText (Text);
-    this.Image = new Gwt.Gui.Image (Image);
-    
-    this.SetSize (Width, Height);
-    this.SetBorderColor (new Gwt.Gui.Contrib.Color (20, 20, 25, 0.9));
-    this.SetBorderBottom (1);
-    
-    this.Layout.SetSize (this.GetWidth(), this.GetHeight ());
-    this.Layout.SetAlignment (Gwt.Gui.ALIGN_CENTER);
-    this.Add (this.Layout);
-    
-    this.Text.SetExpand (false);
-    this.Text.SetWidth (this.GetWidth () - 80, 24);
-    this.Text.SetMarginLeft (12);
-    
-    this.Layout.Add (this.Text);
-    
-    this.Image.SetSize (32, 32);
-    this.Image.SetMarginRight (24);
-    this.Layout.Add (this.Image);
-}
-PanelItem.prototype = new Gwt.Gui.Frame ();
-PanelItem.prototype.constructor = PanelItem;
-
-PanelItem.prototype._PanelItem = function ()
-{
-    this.Text._StaticText();
-    this.Image._Image();
-    this.Layout._HBox();
-    
-    this.Text = null;
-    this.Image = null;
-    this.Layout = null;
-    
-    this._Frame ();
-}
-
 var instance;
 
-function domotictrl () 
+function login () 
 {
-    Gwt.Gui.Window.call (this, "Domótica");
+    Gwt.Gui.Window.call (this);
+
+    //instace props
+    this.imageLogin = new Gwt.Gui.Image(Gwt.Core.Contrib.Images+"connecting_world.svg");
+    this.title_label = new Gwt.Gui.StaticText ("Login");
+    this.id_type_select = new Gwt.Gui.SelectBox ("Tipo de Documento", [{"text": "Tarjeta de Identidad", "value": "T.I"}, {"text": "Cédula de Ciudadanía", "value": "C.C"}, {"text": "Registro Civil", "value": "R.C"}, {"text": "Cédula Extranjera", "value": "C.E"}, {"text": "Pasaporte", "value": "PS"}, {"text": "Libreta Militar", "value": "L.M"}, {"text": "Registro de Defunción", "value": "R.D"}, {"text": "Carnét de Salud", "value": "C.S"}, {"text": "Registro Mercantil", "value": "R.M"}]);
+    this.username_entry = new Gwt.Gui.Entry ("Número de Documento");
+    this.password_entry = new Gwt.Gui.Entry ("Contraseña");
+    this.send_button = new Gwt.Gui.Button (Gwt.Core.Contrib.Images+"appbar.arrow.right.svg", "Entrar");
+    this.controls_container = new Gwt.Gui.VBox ();
     
-    var WIDTH = 640;
-    var HEIGHT = 320;
-    
-    this.Layout = new Gwt.Gui.HBox(0);
-    this.Col1 = new Gwt.Gui.VBox (0);
-    this.Col2 = new Gwt.Gui.VBox (0);
-    this.Knob = new  Gwt.Gui.KnobThreeLevels ();
-    this.Title = new Gwt.Gui.StaticText ("Ventilador");
-    this.PanelCtrlHead = new Gwt.Gui.Frame ();
-    this.PanelCtrlHeadBox = new Gwt.Gui.VBox (0);
-    this.PanelCtrlTilte = new Gwt.Gui.StaticText ("Panel De Control");
-    this.VentiladorPanelCtrl = null;
-    this.VentanasPanelCtrl = null;
-    this.LuzPanelCtrl = null;
-    this.PersianasPanelCtrl = null;
-        
-    this.SetSize (WIDTH, HEIGHT);
+    //init
+    this.SetSize (Gwt.Gui.SCREEN_DEVICE_WIDTH - 50, Gwt.Gui.SCREEN_DEVICE_HEIGHT - 50);
     this.SetPosition (Gwt.Gui.WIN_POS_CENTER);
-    this.DisableMenu ();
+    this.DisableTitleBar ();
+	
+    this.imageLogin.SetSize (500, 350);
+    this.imageLogin.SetPosition (170, 180);
+    this.imageLogin.SetPositionType (Gwt.Gui.Contrib.PositionType.Absolute);
+    this.Add (this.imageLogin);
     
-    this.Layout.SetSize (this.GetAvailableWidth (), this.GetAvailableHeight());    
-    this.Add (this.Layout);
+    this.controls_container.SetSize (180, 170);
+    this.controls_container.SetPosition ((this.GetWidth()*70)/100, ((this.GetHeight()*50)/100)-(this.controls_container.GetHeight()/2));
+    this.Add (this.controls_container);
     
-    this.Col1.SetBackgroundImage (Gwt.Core.Contrib.Images + "blurry_background2_33.png");
-    this.Col1.SetBackgroundSize (Gwt.Gui.Contrib.BackgroundSize.Cover);
-    this.Col1.SetAlignment (Gwt.Gui.ALIGN_CENTER);
-    this.Layout.Add (this.Col1);    
+    this.controls_container.Add (this.title_label);
+    this.controls_container.Add (this.id_type_select);
     
-    this.Title.SetSize (320, 32);
-    this.Title.SetExpand (false);
-    this.Title.SetTextAlignment (Gwt.Gui.Contrib.TextAlign.Center);
-    this.Title.SetMarginTop (25);
-    this.Title.SetFontSize (20);
-    this.Col1.Add (this.Title);
+    this.username_entry.SetFocus ();
+    this.controls_container.Add (this.username_entry);
     
-    this.Knob.SetMarginTop (16);
-    this.Col1.Add (this.Knob);
-    
-    this.Col2.SetBackgroundColor (new Gwt.Gui.Contrib.Color (50, 50, 50, 0.9));
-    this.Layout.Add (this.Col2);
-    
-    this.PanelCtrlHead.SetExpand (true);
-    this.PanelCtrlHead.SetHeight (56);
-    this.PanelCtrlHead.SetBackgroundColor (new Gwt.Gui.Contrib.Color (40, 40, 45, 0.9));
-    this.PanelCtrlHead.SetBorderColor (new Gwt.Gui.Contrib.Color (20, 20, 25, 0.9));
-    this.PanelCtrlHead.SetBorder (1);
-    this.PanelCtrlHead.SetBorderBottom (2);
-    this.Col2.Add (this.PanelCtrlHead);
-    
-    this.PanelCtrlHeadBox.SetSize (this.PanelCtrlHead.GetWidth (), this.PanelCtrlHead.GetHeight ());
-    this.PanelCtrlHead.Add (this.PanelCtrlHeadBox);
-    
-    this.PanelCtrlTilte.SetExpand (true);
-    this.PanelCtrlTilte.SetTextAlignment (Gwt.Gui.Contrib.TextAlign.Center);
-    this.PanelCtrlTilte.SetFontSize (16);
-    this.PanelCtrlTilte.SetHeight (32);
-    this.PanelCtrlTilte.SetMarginTop (10);
-    this.PanelCtrlHeadBox.Add (this.PanelCtrlTilte);
-    
-    this.VentiladorPanelCtrl = new PanelItem (this.Col2.GetWidth(), 56, "Control De Ventilador", Gwt.Core.Contrib.Images + "appbar.fan.box.svg");
-    this.Col2.Add (this.VentiladorPanelCtrl);
-    
-    this.VentanasPanelCtrl = new PanelItem (this.Col2.GetWidth(), 56, "Control De Ventanas", Gwt.Core.Contrib.Images + "appbar.window.restore.svg");
-    this.Col2.Add (this.VentanasPanelCtrl);
-    
-    this.LuzPanelCtrl = new PanelItem (this.Col2.GetWidth(), 56, "Control De Luz", Gwt.Core.Contrib.Images + "appbar.lightbulb.coil.svg");
-    this.Col2.Add (this.LuzPanelCtrl);
-    
-    this.PersianasPanelCtrl = new PanelItem (this.Col2.GetWidth(), 56, "Control De Persianas", Gwt.Core.Contrib.Images + "appbar.map.folds.svg");
-    this.Col2.Add (this.PersianasPanelCtrl);
+    this.password_entry.ChangeToPassword ();
+    this.password_entry.SetMaxLength (4);
+    this.controls_container.Add (this.password_entry);
+	
+    this.send_button.SetWidth (80);
+    this.send_button.AddEvent (Gwt.Gui.Event.Mouse.Click, this.send.bind (this));
+    this.controls_container.Add (this.send_button);
+
 }
 
-domotictrl.prototype = new Gwt.Gui.Window ();
-domotictrl.prototype.constructor = domotictrl;
+login.prototype = new Gwt.Gui.Window ();
+login.prototype.constructor = login;
 
-domotictrl.prototype._app = function ()
+login.prototype._App = function ()
 {
+    this.imageLogin._Image ();
+    this.title_label._StaticText ();
+    this.id_type_select._SelectBox ();
+    this.username_entry._Entry ();
+    this.password_entry._Entry ();
+    this.send_button._Button ();
+    this.controls_container._VBox ();
     
+    this.imageLogin = null;
+    this.title_label = null;
+    this.id_type_select = null;
+    this.username_entry = null;
+    this.password_entry = null;
+    this.send_button = null;
+    this.controls_container = null;
+}
+
+login.prototype.send = function ()
+{
+    if (this.username_entry.GetText () !== "" && this.password_entry.GetText () !== "")
+    {
+	var password = new jsSHA(this.password_entry.GetText (), "TEXT").getHash ("SHA-256", "HEX");
+	new Gwt.Core.Request ("/backend/auth/", {'username': this.username_entry.GetText (), 'password': password}, this.response.bind(this));
+    }
+    else
+    {
+	console.log ("Datos vacíos");
+    }
+}
+
+login.prototype.response = function (data)
+{
+    if (data.status === "success")
+    {
+        if (Boolean (Number (data.response)))
+	{
+            start_up_env (this.username_entry.GetText ());
+	}
+    }
+    else
+    {
+	console.log (data);
+    }
+}
+	
+return new function ()
+{
+    this.open = function ()
+    {
+        if (instance === undefined)
+	{
+            instance = new login ();
+            instance.Open ();
+	}
+	else
+	{
+            console.log ("%app open".replace ("%app", instance.__proto__.constructor.name));
+	}
+    }
+		
+    this.close = function ()
+    {
+	if (instance !== undefined)
+	{
+            instance.Close ();
+            instance = undefined;
+	} 
+    }
+}
+})();
+block = (function ()
+{ 
+var instance;
+
+function block ()
+{
+    Gwt.Gui.Window.call (this, "Sessión Bloqueada");
+
+    //instance props
+    this.clock = new Gwt.Gui.Clock ();
+    this.date = null;
+    this.unlock_button = new Gwt.Gui.Button (Gwt.Core.Contrib.Images+"document-decrypt.svg", "Desbloquear");
+    this.layout = new Gwt.Gui.VBox();
+    
+    this.SetSize (250,330);
+    this.SetPosition (Gwt.Gui.WIN_POS_CENTER);
+	
+    var date = new Date ();
+
+    var days = [ 'Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sáb'];
+    var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+    this.date = new Gwt.Gui.StaticText ("%d, %m %n, %y".replace ("%d", days[date.getDay ()]).replace ("%m", months[date.getMonth ()]).replace ("%n", date.getDate ()).replace ("%y", date.getFullYear ()));
+    this.date.SetWidth (180);
+    this.date.TextAlign ("center");
+
+    this.unlock_button.SetWidth (120);
+
+    this.layout.SetAlignment(Gwt.Gui.ALIGN_CENTER);
+	
+    this.Add(this.layout);
+    this.SetBorderSpacing (12);
+    this.layout.Add(this.clock);
+    this.layout.Add(this.date);
+    this.layout.Add(this.unlock_button);
+		
+    this.unlock_button.AddEvent (Gwt.Gui.Event.Mouse.Click, this.unlock.bind(this));
+}
+
+block.prototype = new Gwt.Gui.Window ();
+block.prototype.constructor = block;
+
+block.prototype._App = function ()
+{
+    this.clock._Clock ();
+    this.date._StaticText ();
+    this.unlock_button._Button ();
+    this.layout._VBox ();
+    
+    this.clock = null;
+    this.date = null;
+    this.unlock_button = null;
+    this.layout = null;
+}
+
+block.prototype.unlock = function ()
+{
+    unlock_session ();
+}
+	
+return new function ()
+{
+    this.open = function ()
+    {
+        if (instance === undefined)
+        {
+            instance = new block ();
+            instance.Open ();
+        }
+        else
+        {
+            console.log ("%app open".replace ("%app", instance.__proto__.constructor.name));
+        }
+    }
+	
+    this.close = function ()
+    {
+        if (instance !== undefined)
+        {
+            instance.Close ();
+            instance = undefined;
+        } 
+    }
+}
+	
+})();
+gcontrol = ( function ()
+{
+var instance;
+
+function gcontrol () 
+{
+    Gwt.Gui.Window.call (this);
+
+    this.layout = new Gwt.Gui.VBox(5);
+    this.icons = [
+        new Gwt.Gui.IconDesktop (Gwt.Core.Contrib.Images + "preferences-desktop-user.png", "Usuarios", function(){window.gcontrol.close(); window.gusers.open();})
+    ];    
+    
+    this.SetSize (512, 512);
+    this.SetPosition (Gwt.Gui.WIN_POS_CENTER);
+    this.DisableTitleBar ();
+
+    this.layout.SetSize (this.GetWidth(), this.GetHeight());
+    this.Add (this.layout);
+    this.SetBorderSpacing (12);
+    
+    for (var i = 0; i < this.icons.length; i++)
+    {
+        this.layout.Add (this.icons[i]);
+    }
+}
+
+gcontrol.prototype = new Gwt.Gui.Window ();
+gcontrol.prototype.constructor = gcontrol;
+
+gcontrol.prototype._App = function ()
+{
+    for (var i = 0; i < this.icons.length; i++)
+    {
+        this.icons[i]._IconDesktop ();
+    }
+    this.layout._VBox ();
+    
+    this.layout = null;
+    this.icons  = null;
+}
+
+return new function ()
+{
+	this.open = function ()
+	{
+		if (instance === undefined)
+		{
+			instance = new gcontrol ();
+                        instance.Open ();
+		}
+		else
+		{
+			console.log ("%app open".replace ("%app", instance.__proto__.constructor.name));
+		}
+                
+	}
+		
+	this.close = function ()
+	{
+            
+		if (instance !== undefined)
+		{
+			instance.Close ();
+			instance = undefined;
+		} 
+                
+	}
+}
+})();
+
+gusers = ( function ()
+{
+var instance;
+
+function gusers () 
+{
+    Gwt.Gui.Window.call (this, "Usuarios");
+	
+    this.SetSize (320, 440);
+    this.SetPosition (Gwt.Gui.WIN_POS_CENTER);
+   
+    this.EnableMenu ();
+    this.AddMenuItem (Gwt.Core.Contrib.Images + "appbar.magnify.svg", "Buscar", this.Buscar.bind(this));
+    this.AddMenuItem (Gwt.Core.Contrib.Images + "appbar.cabinet.in.svg", "Guardar", this.Guardar.bind(this));
+    this.AddMenuItem (Gwt.Core.Contrib.Images + "appbar.refresh.svg", "Actualizar", this.Actualizar.bind(this));
+    this.AddMenuItem (Gwt.Core.Contrib.Images + "appbar.delete.svg", "Eliminar", this.Eliminar.bind(this));
+    this.AddMenuItem (Gwt.Core.Contrib.Images + "appbar.power.svg", "Salir", function(){window.gusers.close(); window.gcontrol.open();}, Gwt.Gui.MENU_QUIT_APP);
+    
+    this.layout = new Gwt.Gui.VBox ();
+    this.layout.SetAlignment(Gwt.Gui.ALIGN_CENTER);
+    this.Add (this.layout);
+    this.SetBorderSpacing (12);
+	
+    this.avatar = new Gwt.Gui.Avatar ("Avatar");
+    this.avatar.SetSizeEditor (this.GetWidth(), this.GetHeight()-32);
+    this.title = new Gwt.Gui.StaticText ("Datos:");
+    this.doc_type = new Gwt.Gui.IconSelectBox (Gwt.Core.Contrib.Images+"appbar.notification.star.svg", "Tipo de Documento", [{"text": "Tarjeta de Identidad", "value": "T.I"}, {"text": "Cédula de Ciudadanía", "value": "C.C"}, {"text": "Registro Civil", "value": "R.C"}, {"text": "Cédula Extranjera", "value": "C.E"}, {"text": "Pasaporte", "value": "PS"}, {"text": "Libreta Militar", "value": "L.M"}]);
+    this.doc_num = new Gwt.Gui.IconEntry(Gwt.Core.Contrib.Images+"appbar.notification.svg", "Número de Documento");
+    this.name = new Gwt.Gui.IconEntry(Gwt.Core.Contrib.Images+"appbar.user.tie.svg", "Nombre");
+    this.last_name = new Gwt.Gui.IconEntry(Gwt.Core.Contrib.Images+"appbar.user.add.svg", "Apellidos");
+    this.phone = new Gwt.Gui.IconEntry(Gwt.Core.Contrib.Images+"appbar.phone.svg", "Teléfono");
+    this.email = new Gwt.Gui.IconEntry(Gwt.Core.Contrib.Images+"appbar.email.minimal.svg", "Correo Electrónico");
+    this.address = new Gwt.Gui.IconEntry(Gwt.Core.Contrib.Images+"appbar.home.location.round.svg", "Dirección de Residencia");
+        
+    this.layout.Add (this.avatar);
+    this.layout.Add (this.title);
+    this.layout.Add (this.doc_type);
+    this.layout.Add (this.doc_num);
+    this.layout.Add (this.name);
+    this.layout.Add (this.last_name);
+    this.layout.Add (this.phone);
+    this.layout.Add (this.email);
+    this.layout.Add (this.address);
+    
+    this.Add (this.avatar.GetEditor ());
+}
+
+gusers.prototype = new Gwt.Gui.Window ();
+gusers.prototype.constructor = gusers;
+
+gusers.prototype._app = function ()
+{
+    this.layout.FinalizeVBox ();
+    this.layout = null;
+    
+    this.avatar.FinalizeAvatar ();
+    this.avatar = null;
+    
+    this.croppie.FinalizeAvatar ();
+    this.croppie = null;
+    
+    this.title.FinalizeStaticText ();
+    this.title = null;
+    
+    this.doc_type.FinalizeIconSelectBox ();
+    this.doc_type = null;
+    
+    this.doc_num.FinalizeIconEntry ();
+    this.doc_num = null;
+    
+    this.name.FinalizeIconEntry ();
+    this.name = null;
+    
+    this.last_name.FinalizeIconEntry ();
+    this.last_name = null;
+    
+    this.phone.FinalizeIconEntry ();
+    this.phone = null;
+    
+    this.email.FinalizeIconEntry ();
+    this.email = null;
+    
+    this.address.FinalizeIconEntry ();
+    this.address = null;
+}
+
+gusers.prototype.Buscar = function ()
+{
+    console.log ("Buscar");
+}
+
+gusers.prototype.Guardar = function ()
+{
+    var params = [
+        new Gwt.Core.Parameter(Gwt.Core.PARAM_TYPE_FILE, this.avatar.GetData ()),
+        new Gwt.Core.Parameter(Gwt.Core.PARAM_TYPE_JSON, {"Name": "Data", "Data": [{"DocumentType": this.doc_type.GetText(), "DocumentNum": this.doc_num.GetText(), "Name": this.name.GetText(), "LastName": this.last_name.GetText(), "Phone": this.phone.GetText(), "Email": this.email.GetText (), "Address": this.address.GetText ()}]})
+    ];
+    
+    new Gwt.Core.Request ("/backend/gusers/save/", function(response){console.log(response)}, params);
+}
+
+gusers.prototype.Actualizar = function ()
+{
+    console.log ("Actualizar");
+}
+
+gusers.prototype.Eliminar = function ()
+{
+    console.log ("Eliminar");
 }
 
 return new function ()
@@ -182,7 +442,7 @@ return new function ()
     {
         if (instance === undefined)
         {
-            instance = new domotictrl ();
+            instance = new gusers ();
             instance.Open ();
         }
         else
@@ -201,3 +461,4 @@ return new function ()
     }
 }
 })();
+
--- frontend/bin/make_bin.sh
+++ frontend/bin/make_bin.sh
@@ -2,11 +2,11 @@
 
 echo "making bin.js"
 cat desktop.js > bin.js
-#cat login.js >> bin.js
-#cat block.js >> bin.js
-#cat gcontrol.js >> bin.js
-#cat gusers.js >> bin.js
-cat domotictrl.js >> bin.js
+cat login.js >> bin.js
+cat block.js >> bin.js
+cat gcontrol.js >> bin.js
+cat gusers.js >> bin.js
+#cat domotictrl.js >> bin.js
 
 yuicompressor.sh bin.js
 echo "bin.js maked"
