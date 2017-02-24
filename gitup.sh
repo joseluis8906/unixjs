fd9a24ab493a46a8e276990e21edc7f59a04e72d
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-02-24 12:54:15 -0500

    update

--- gitup.sh
+++ gitup.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-function compile {
-    cd ./frontend/
-    ./make_unixjs.sh
-    cd ../
-}
-
-if [ "$1" = "local" ];
-then
-    compile
-    git add .
-    git commit -m "update"
-elif [ "$1" = "remote" ];
-then
-    git push origin master
-fi
+git add .
+git commit -m "update"
+git push origin master
