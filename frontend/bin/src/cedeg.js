a7250a943bb064c0255206c3b0ac779992a86352
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-03-01 16:40:09 -0500

    update

--- frontend/bin/src/cedeg.js
+++ frontend/bin/src/cedeg.js
@@ -245,8 +245,8 @@ cedeg.prototype.SaveResponse = function (Res)
 
 cedeg.prototype.Delete = function ()
 {
-    var Stm = new Gwt.Core.PrepareStatement ("DELETE FROM \"AccountingDisbVou\" WHERE \"Number\"=?").SetNumber(this.number.GetText ());
-    console.log (Stm.GetText());
+    var Stm = new Gwt.Core.PrepareStatement ("DELETE FROM \"AccountingDisbVou\" WHERE \"Number\"=?");
+    Stm.SetNumber(this.number.GetText ());
     
     new Gwt.Core.SqlStatement ([Stm], this.DeleteResponse.bind(this));
 };
