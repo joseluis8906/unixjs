595d3d6ceee7dce12a2eacbecdd639937d31e2e9
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-03-22 19:25:23 -0500

    actualizado

--- sbin/contrib/session.lua
+++ sbin/contrib/session.lua
@@ -5,7 +5,7 @@
 local Http = require ("./sbin/contrib/http");
 local Crypt = require ("./sbin/contrib/crypt");
 
-ngx.log(ngx.ERR, ngx.var[1]);
+ngx.log(ngx.ERR, table.getn(ngx.var));
 --GenerateSessionId
 function GenUid (PseudoId)
     local Dict = "0X1P2QV4cCUdeAfgMhijEklmnS5OpZ@qrKsWt9vIw7by6zBu-DF3Hx8JaL_NRoTYG";
