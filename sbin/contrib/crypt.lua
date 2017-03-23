a4e72fc99737ca58a203efeb041795197d33808d
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-03-23 08:25:43 -0500

    actualizado

--- sbin/contrib/crypt.lua
+++ sbin/contrib/crypt.lua
@@ -2,7 +2,7 @@
     Lua 5.1 Copyright (C) 1994-2006 Lua.org, PUC-Rio
 ]]
 --package.path = ngx.var.app.."/?.lua;"..package.path
-ngx.log(ngx.ERR, packege.path);
+ngx.log(ngx.ERR, package.path);
 local App = ngx.var.app;
 local Sha2 = require ("contrib/sha2");
 
