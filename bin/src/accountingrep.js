32526d47b6ef9ff2728ca7f74ed378ad3e45f06e
Jose Cáceres <joseluis8906@opmbx.org>
2017-04-05 08:36:55 -0500

    actualizado

--- bin/src/accountingrep.js
+++ bin/src/accountingrep.js
@@ -88,7 +88,7 @@ accountingrep.prototype.Print = function ()
 //print response
 accountingrep.prototype.PrintResponse = function (Res)
 {
-    var Registros = Res.Notes;
+    var Registros = Res.DisbVous;
     this.Records = this.SortData (Registros);
     this.Report = Gwt.Core.Contrib.LoadDocument ("/documents/daily.html?records=%0".replace("%0", this.Records.length));
     this.Report.addEventListener ("load", this.ReportLoad.bind (this));
