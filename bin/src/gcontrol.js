be6201708d7355158b0c7ef457c5c59bee4960f6
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-04-21 18:58:06 -0500

    actualizado

--- bin/src/gcontrol.js
+++ bin/src/gcontrol.js
@@ -2,26 +2,30 @@ gcontrol = ( function ()
 {
 var instance;
 
-function gcontrol () 
+function gcontrol ()
 {
     Gwt.Gui.Window.call (this);
-    
+
     this.Rpc = new Gwt.Core.Rpc ("/gcontrol/");
     this.Layout = new Gwt.Gui.VBox(5);
     this.Row1 = new Gwt.Gui.HBox(8);
+    this.Row2 = new Gwt.Gui.HBox(8);
+    this.Row3 = new Gwt.Gui.HBox(8);
+    this.Row4 = new Gwt.Gui.HBox(8);
+    this.Row5 = new Gwt.Gui.HBox(8);
     this.Icons = [];
-        
+
     this.DisableTitleBar ();
     this.SetSize (544, 544);
     this.SetPosition (Gwt.Gui.WIN_POS_CENTER);
     this.SetBorderSpacing (12);
-    
+
     this.Layout.SetSize (this.GetAvailableWidth(), this.GetAvailableHeight());
     this.SetLayout (this.Layout);
-    
+
     this.Row1.SetAlignment (Gwt.Gui.ALIGN_TOP);
     this.Layout.Add (this.Row1);
-    
+
     this.Rpc.Send({Method: "Select"}, this.SelectResponse.bind(this));
 }
 
@@ -36,7 +40,7 @@ gcontrol.prototype._App = function ()
     }
     this.Row1._HBox ();
     this.Layout._VBox ();
-    
+
     this.Layout = null;
     this.Icons  = null;
 }
@@ -48,23 +52,42 @@ gcontrol.prototype.SelectResponse = function (Res)
     for (var i = 0; i < Res.length; i++)
     {
         this.Icons.push (new Gwt.Gui.IconDesktop (Gwt.Core.Contrib.Images+Res[i].Image, Res[i].Label, Res[i].Name));
-        this.Row1.Add (this.Icons[i]);
+        if (i<5)
+        {
+          this.Row1.Add (this.Icons[i]);
+        }
+        else if (i >= 5 && i < 10)
+        {
+          this.Row2.Add (this.Icons[i]);
+        }
+        else if (i >= 10 && i < 15)
+        {
+          this.Row3.Add (this.Icons[i]);
+        }
+        else if (i >= 15 && i < 20)
+        {
+          this.Row4.Add (this.Icons[i]);
+        }
+        else if (i >= 20 && i < 25)
+        {
+          this.Row5.Add (this.Icons[i]);
+        }
         this.LoadApp (Res[i].Name);
     }
 }
 
 
 
-gcontrol.prototype.LoadApp = function (App) 
+gcontrol.prototype.LoadApp = function (App)
 {
     var TagScript = document.createElement('script');
     TagScript.type = 'text/javascript';
     TagScript.async = true;
     TagScript.src = "/bin/"+App+".min.js";
     var Head = document.head;
-    
+
     var Insert = true;
-    
+
     for (var i = 0; i < Head.childNodes.length; i++)
     {
         if (Head.childNodes[i].src === TagScript.src)
@@ -72,7 +95,7 @@ gcontrol.prototype.LoadApp = function (App)
             Insert = false;
         }
     }
-    
+
     if (Insert === true)
     {
         Head.appendChild(TagScript);
@@ -95,9 +118,9 @@ return new function ()
             {
                 console.log ("%app opened yet".replace ("%app", instance.__proto__.constructor.name));
             }
-                
+
 	};
-		
+
 	this.close = function ()
 	{
             if (instance !== undefined)
@@ -105,8 +128,7 @@ return new function ()
                 instance.Close();
                 instance = undefined;
                 Gwt.Core.Contrib.RemoveActiveApp ();
-            }   
+            }
 	};
 };
 })();
-
