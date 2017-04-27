0dc19a5800170f4c835f9abb1e85875e56326e3f
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-04-26 20:40:49 -0500

    actualizado

--- bin/src/accountingnotes.js
+++ bin/src/accountingnotes.js
@@ -206,7 +206,7 @@ function accountingnotes()
 {
     Gwt.Gui.Window.call (this, "Nota Contable");
 
-    this.SetSize (840, 532);
+    this.SetSize (840, 542);
     this.SetPosition (Gwt.Gui.WIN_POS_CENTER);
     this.SetBorderSpacing (12);
     this.Rpc = new Gwt.Core.Rpc ("/accountingnotes/");
