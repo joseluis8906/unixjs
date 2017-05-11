a0851817b1b6e395176ab97cd42e3b753dcbd417
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-05-10 22:42:49 -0500

    actualizado

--- bin/src/gcontrol.js
+++ bin/src/gcontrol.js
@@ -57,6 +57,7 @@ gcontrol.prototype._App = function ()
 
 gcontrol.prototype.SelectResponse = function (Res)
 {
+    console.log (Gwt.Core.Apps.length);
     for (var i = 0; i < Gwt.Core.Apps.length; i++)
     {
         Gwt.Core.Apps.pop ();
