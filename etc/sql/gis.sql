02c6b64118a2d748fb14416862e7854b95b6ac42
Jose Luis Cáceres Escudero <joseluis8906@opmbx.org>
2017-05-18 00:02:55 -0500

    actualizado

---
+++ etc/sql/gis.sql
@@ -0,0 +1,80 @@
+CREATE SCHEMA IF NOT EXISTS "Gis";
+
+CREATE TABLE IF NOT EXISTS "Gis"."Envase"
+(
+    "Id" BIGSERIAL PRIMARY KEY,
+    "Numero" INTEGER NOT NULL UNIQUE,
+    "Numero" INTEGER UNIQUE,
+    "Material" VARCHAR(64),
+    "Capacidad" DECIMAL,
+    "ClaseProducto" VARCHAR(64),
+    "NormaTecnica" VARCHAR(64)
+);
+
+CREATE TABLE IF NOT EXISTS "Gis"."EnvaseComplementaryInfo"
+(
+    "EnvaseId" BIGINT PRIMARY KEY NOT NULL REFERENCES "Gis"."Envase" ("Id") ON UPDATE CASCADE ON DELETE CASCADE,
+    "Presion" DECIMAL,
+    "AlturaConValvula" DECIMAL,
+    "PesoConValvula" DECIMAL,
+    "Valvula" BOOLEAN,
+    "TipoValvula" VARCHAR(64),
+    "AcabadoColor" VARCHAR(128)
+);
+
+CREATE TABLE IF NOT EXISTS "Gis"."EnvaseGeneralidades"
+(
+    "EnvaseId" BIGINT PRIMARY KEY NOT NULL REFERENCES "Gis"."Envase" ("Id") ON UPDATE CASCADE ON DELETE CASCADE,
+    "Proveedor" VARCHAR(128),
+    "FechaCompra" DATE,
+    "Garantia" BOOLEAN,
+    "FechaFabricacion" DATE,
+    "PruebaHidrostatica" BOOLEAN,
+    "Alquilado" BOOLEAN,
+    "FechaAlquiler" DATE,
+    "Observaciones" VARCHAR(512)
+);
+
+CREATE TABLE IF NOT EXISTS "Gis"."Produccion1"
+(
+    "EnvaseId" BIGINT PRIMARY KEY NOT NULL REFERENCES "Gis"."Envase" ("Id") ON UPDATE CASCADE ON DELETE CASCADE,
+    "FechaFabricacion" DATE,
+    "FechaVencimiento" DATE,
+    "Lote" VARCHAR(16),
+    "Cantidad" DECIMAL,
+    "HoraInicial" TIME
+);
+
+CREATE TABLE IF NOT EXISTS "Gis"."Produccion2"
+(
+    "EnvaseId" BIGINT PRIMARY KEY NOT NULL REFERENCES "Gis"."Envase" ("Id") ON UPDATE CASCADE ON DELETE CASCADE,
+    "HoraFinal" TIME,
+    "Turno" VARCHAR(16),
+    "Observaciones" VARCHAR(512),
+    "PurezaFinal" DECIMAL,
+    "PresionFinal" DECIMAL
+);
+
+CREATE TABLE IF NOT EXISTS "Gis"."Cliente"
+(
+    "Id" BIGSERIAL PRIMARY KEY,
+    "NitCC" VARCHAR(32),
+    "Nombre" VARCHAR(128)
+    "Direccion" VARCHAR(128),
+    "Telefono" VARCHAR(16)
+);
+
+CREATE TABLE IF NOT EXISTS "Gis"."Remision"
+(
+    "ClienteId" BIGINT PRIMARY KEY NOT NULL REFERENCES "Gis"."Cliente" ("Id") ON UPDATE CASCADE ON DELETE CASCADE,
+    "Numero" BIGINT NOT NULL,
+    "Fecha" DATE
+);
+
+CREATE TABLE IF NOT EXISTS "Gis"."RemisionRecord"
+(
+    "RemisionId" BIGINT PRIMARY KEY NOT NULL REFERENCES "Gis"."Remision" ("Id") ON UPDATE CASCADE ON DELETE CASCADE,
+    "EnvaseId" BIGINT PRIMARY KEY NOT NULL REFERENCES "Gis"."Envase" ("Id") ON UPDATE CASCADE ON DELETE CASCADE,
+    "Entra" INTEGER,
+    "Sale" INTEGER
+);
